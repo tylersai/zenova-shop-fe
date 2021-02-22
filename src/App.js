507499f70543c98612da1b01dd3aa1efbcec79d6
Sai Han <thesaihan@gmail.com>
2021-02-22 21:21:12 +0630

    Install bootswatch's LUX theme css and show hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Col, Container, Row } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <h2 className="text-center mt-4">Welcome to Zenova Shop</h2>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
