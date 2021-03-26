bf0092bc8847e98dcf9e6b45ed3568c162aa712b
Sai Han <thesaihan@gmail.com>
2021-03-26 21:53:16 +0630

    ActionType constants

--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -1,12 +1,14 @@
+import { ActionType } from "../constants";
+
 export const productListReducer = (state = { productList: [] }, action) => {
   switch (action.type) {
-    case "PRODUCT_LIST_REQUEST":
+    case ActionType.PRODUCT_LIST_REQUEST:
       return { loading: true, productList: [] };
 
-    case "PRODUCT_LIST_SUCCESS":
+    case ActionType.PRODUCT_LIST_SUCCESS:
       return { loading: false, productList: action.payload };
 
-    case "PRODUCT_LIST_FAIL":
+    case ActionType.PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
 
     default:
