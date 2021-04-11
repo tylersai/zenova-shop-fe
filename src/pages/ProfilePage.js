60d0b519cc84cbd4636f21124dae9ff6e3c9cfb1
Sai Han <thesaihan@gmail.com>
2021-04-11 17:22:35 +0630

    Display profile data from currently logged in user

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Col, Row } from "reactstrap";
 import { ProfileCard } from "./profile";
 
 export const ProfilePage = () => {
+  const { data } = useSelector((state) => state.currentUserState);
   return (
     <div className="ProfilePage">
       <Row>
         <Col xs={12} md={4}>
-          <ProfileCard name="John Doe" email="john@gmail.com" />
+          {data && data._id && (
+            <ProfileCard name={data.name} email={data.email} />
+          )}
         </Col>
         <Col xs={12} md={8}>
           <h3>Orders</h3>
