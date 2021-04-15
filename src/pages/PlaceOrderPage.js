dfc8bbaf33a9925c701e70cf609f753722a59ab7
Sai Han <thesaihan@gmail.com>
2021-04-15 21:09:36 +0630

    Finalize order summary section

--- src/pages/PlaceOrderPage.js
+++ src/pages/PlaceOrderPage.js
@@ -12,6 +12,24 @@ export const PlaceOrderPage = () => {
   const paymentMethodState = useSelector((state) => state.paymentMethodState);
   const { paymentMethod } = paymentMethodState.data || {};
 
+  const cartState = useSelector((state) => state.cartState);
+  const cartItems = cartState.data || [];
+
+  const getSubtotal = () =>
+    +cartItems
+      .reduce((tot, el) => tot + +el.price.toFixed(2) * +el.qty, 0)
+      .toFixed(2);
+  const subtotal = getSubtotal();
+
+  const getShippingFee = () => +(subtotal >= 100 ? 0 : 50).toFixed(2);
+  const shippingFee = getShippingFee();
+
+  const getTaxAmt = () => +(subtotal * 0.05).toFixed(2);
+  const taxAmt = getTaxAmt();
+
+  const getTotalAmt = () => subtotal + shippingFee + taxAmt;
+  const totalAmt = getTotalAmt();
+
   const goPlaceOrder = (e) => {
     e.preventDefault();
     alert("Placing order...");
@@ -20,12 +38,12 @@ export const PlaceOrderPage = () => {
   return (
     <div className="PlaceOrderPage">
       <CheckoutStepper step={4} />
-      <Row className="pt-3">
-        <Col md={8}>
+      <Row>
+        <Col md={8} className="mt-3">
           <Card>
             <ListGroup flush>
               <ListGroupItem>
-                <h4 className="my-2">Shipping</h4>
+                <h4 className="my-3">Shipping Address</h4>
                 <p className="my-0">{address}</p>
                 <p className="my-0">
                   {city}, {country}
@@ -33,7 +51,7 @@ export const PlaceOrderPage = () => {
                 <p className="my-0">{postalCode}</p>
               </ListGroupItem>
               <ListGroupItem>
-                <h4 className="my-2">Payment Method</h4>
+                <h4 className="my-3">Payment Method</h4>
                 <div className="d-flex align-items-center pt-1 pb-2">
                   <label className="my-0">Method : </label>
                   <img
@@ -47,11 +65,35 @@ export const PlaceOrderPage = () => {
             </ListGroup>
           </Card>
         </Col>
-        <Col md={4}>
+        <Col md={4} className="mt-3">
           <Card>
             <ListGroup flush>
               <ListGroupItem>
-                <h4 className="my-2">Order Summary</h4>
+                <h4 className="my-3">Order Summary</h4>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Subtotal : </Col>
+                  <Col className="text-right">${subtotal.toFixed(2)}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Shipping Fee : </Col>
+                  <Col className="text-right">${shippingFee.toFixed(2)}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Tax : </Col>
+                  <Col className="text-right">${taxAmt.toFixed(2)}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Total : </Col>
+                  <Col className="text-right">${totalAmt.toFixed(2)}</Col>
+                </Row>
               </ListGroupItem>
               <ListGroupItem>
                 <Button block color="dark" onClick={goPlaceOrder}>
