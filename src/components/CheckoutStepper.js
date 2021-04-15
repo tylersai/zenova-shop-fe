d6e7ced77b2d4adc544493e00e327742917c7e39
Sai Han <thesaihan@gmail.com>
2021-04-15 20:35:47 +0630

    CheckoutStepper responsive whitespace of the step items

--- src/components/CheckoutStepper.js
+++ src/components/CheckoutStepper.js
@@ -7,22 +7,22 @@ export const CheckoutStepper = ({ step }) => {
   return (
     <div className="CheckoutStepper px-2 pt-2 pb-3">
       <Nav className="justify-content-center align-items-center">
-        <NavItem active={step === 1}>
+        <NavItem className="mr-0 mr-sm-1 mr-md-3 mr-lg-4" active={step === 1}>
           <LinkContainer to="/login">
             <NavLink disabled={step < 1}>Sign In</NavLink>
           </LinkContainer>
         </NavItem>
-        <NavItem active={step === 2}>
+        <NavItem className="mr-0 mr-sm-1 mr-md-3 mr-lg-4" active={step === 2}>
           <LinkContainer to="/shipping">
             <NavLink disabled={step < 2}>Shipping</NavLink>
           </LinkContainer>
         </NavItem>
-        <NavItem active={step === 3}>
+        <NavItem className="mr-0 mr-sm-1 mr-md-3 mr-lg-4" active={step === 3}>
           <LinkContainer to="/payment">
             <NavLink disabled={step < 3}>Payment</NavLink>
           </LinkContainer>
         </NavItem>
-        <NavItem active={step === 4}>
+        <NavItem className="mr-0 mr-sm-1 mr-md-3 mr-lg-4" active={step === 4}>
           <LinkContainer to="/place-order">
             <NavLink disabled={step < 4}>Place Order</NavLink>
           </LinkContainer>
