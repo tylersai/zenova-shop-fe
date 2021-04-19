557abc57ca17413b4ac3589094ff45c7d3897358
Sai Han <thesaihan@gmail.com>
2021-04-19 16:59:15 +0630

    Order list styling

--- src/pages/profile/OrderList.js
+++ src/pages/profile/OrderList.js
@@ -8,38 +8,46 @@ export const OrderList = ({ orders = [] }) => {
     <div className="OrderList">
       <h3>Orders</h3>
       {orders && orders.length > 0 ? (
-        <ListGroup flush>
+        <ListGroup flush className="hover">
           {orders.map((o) => (
-            <ListGroupItem key={o._id}>
+            <ListGroupItem key={o._id} action>
               <Row>
                 <Col xs={9} md={4}>
                   <p>
                     <Link to={`/order/${o._id}`}>{o._id}</Link>
                   </p>
                 </Col>
-                <Col xs={3} md={2} className="text-right">
+                <Col xs={3} md={2} className="text-right font-weight-bold">
                   <p>${formatMoney(o.totalAmount)}</p>
                 </Col>
                 <Col xs={6} md={3} className="text-right">
                   {o.isPaid ? (
-                    <p className="text-info">
-                      <span>Paid On</span>
+                    <p className="text-success mb-0">
+                      <span>
+                        <i className="far fa-check-circle"></i> Paid On
+                      </span>
                       <br />
                       <span>{o.paidAt.substr(0, 10)}</span>
                     </p>
                   ) : (
-                    <p className="text-danger">Not Paid</p>
+                    <p className="text-danger mb-0">
+                      <i className="far fa-times-circle"></i> Not Paid
+                    </p>
                   )}
                 </Col>
                 <Col xs={6} md={3} className="text-right">
                   {o.isDelivered ? (
-                    <p className="text-info">
-                      <span>Delivered On</span>
+                    <p className="text-info mb-0">
+                      <span>
+                        <i className="far fa-check-circle"></i> Delivered On
+                      </span>
                       <br />
                       <span>{o.deliveredAt.substr(0, 10)}</span>
                     </p>
                   ) : (
-                    <p className="text-danger">Not Delivered</p>
+                    <p className="text-danger mb-0">
+                      <i className="far fa-times-circle"></i> Not Delivered
+                    </p>
                   )}
                 </Col>
               </Row>
