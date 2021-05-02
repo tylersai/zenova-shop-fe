9d0754dbd8b9a0ea299681649e4d63e3ce08f46d
Sai Han <thesaihan@gmail.com>
2021-05-02 17:13:26 +0630

    Add Customer Orders menu item

--- src/components/Header.js
+++ src/components/Header.js
@@ -77,6 +77,9 @@ export const Header = () => {
                     <DropdownItem onClick={() => history.push("/products")}>
                       Product List
                     </DropdownItem>
+                    <DropdownItem onClick={() => history.push("/orders")}>
+                      Customer Orders
+                    </DropdownItem>
                   </DropdownMenu>
                 </UncontrolledDropdown>
               ) : (
